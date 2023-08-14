d79e701236d9ba3e0f3d26a887b67494289ba636
Seil Jun <ypd0709@naver.com>
2023-08-14 22:57:01 +0900

    23.08.14

--- imdb/test.py
+++ imdb/test.py
@@ -1,30 +1,7 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-stack = []
-
-n = int(input())
-for _ in range(n):
-    x = input().rstrip()
-    if len(x) > 2:
-        stack.append(int(x[2:]))
-    else:
-        x = int(x)
-        if x == 2:
-            if stack:
-                print(stack.pop())
-            else:
-                print(-1)
-        if x == 3:
-            print(len(stack))
-        if x == 4:
-            if stack:
-                print(0)
-            else:
-                print(1)
-        if x == 5:
-            if stack:
-                print(stack[-1])
-            else:
-                print(-1)
+waiting = deque(map(int, input().split()))
+Q = deque()
